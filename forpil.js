602d905b7379f3b67ab6ee800dac9b1f749f3d26
sungwook yeom <sungwook.yeom@samsung.com>
2015-06-17 15:00:40 +0900

    init

---
+++ forpil.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  var days = function() {
+    return Math.floor((new Date() - new Date(2015, 5, 9))/86400000);
+  };
+
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    start: function() {
+      return "2015년 6월 9일(화)";
+    },
+    days: function() {
+      return days();
+    },
+    dday: function(day){
+      return day - days();
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
